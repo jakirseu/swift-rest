69738ba7e6ea3601421cfc66cbbe1033072dc418
Jakir Hossain <jakirseu@gmail.com>
2023-09-27 08:46:56 +0600

    Initial Commit

---
+++ Swift-REST/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-REST
+//
+//  Created by Jakir Hossain on 27/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-REST/Swift_RESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_RESTApp.swift
+//  Swift-REST
+//
+//  Created by Jakir Hossain on 27/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_RESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
